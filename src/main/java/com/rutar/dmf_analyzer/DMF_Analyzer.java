502f7b005853d6246629046b63a9f49196f76f54
Rutar Andriy <RutarAndriy@gmail.com>
2024-06-02 11:08:39 +0300

    Implemented basic functionality

--- src/main/java/com/rutar/dmf_analyzer/DMF_Analyzer.java
+++ src/main/java/com/rutar/dmf_analyzer/DMF_Analyzer.java
@@ -1,105 +1,469 @@
 package com.rutar.dmf_analyzer;
 
-// ............................................................................
+import java.io.*;
+import java.nio.*;
+import java.util.zip.*;
+
+import static java.nio.ByteOrder.*;
 
 /**
  * Клас DMF_Analyzer
  * @author Rutar_Andriy
- * 21.04.2024
+ * 09.04.2024
  */
 
 public class DMF_Analyzer {
 
 ///////////////////////////////////////////////////////////////////////////////
+// Змінні, які реалізують основний функціонал компонента //////////////////////
+
+private Signature           signature;         // Мітка розпізнавання DMF файлу
+private Header_Structure    header_structure;                 // Блок заголовку
+private Layers_Structure    layers_structure;                     // Блок шарів
+private Params_Structure    params_structure;                // Блок параметрів
+private Symbols_Structure   symbols_structure;           // Блок умовних знаків
+private Passwords_Structure passwords_structure;                // Блок паролів
+private Objects_Structure   objects_structure;                 // Блок об'єктів
+
+///////////////////////////////////////////////////////////////////////////////
+// Допоміжні змінні /////////////////////////////////////////// ///////////////
+
+private InputStream is;              // Потік для зчитування даних із DMF файлу
+private final ByteArrayOutputStream temp = new ByteArrayOutputStream(); // доп.
+
+///////////////////////////////////////////////////////////////////////////////
 
 /**
- * Метод підраховує суму вхідних чисел
- * @param numbers масив/перелік вхідних чисел
- * @return сума вхідних чисел
+ * Базовий аналіз DMF файлу
+ * @param file *.dmf файл
+ * @throws Exception якщо в процесі аналізування сталася критична помилка
  */
-public static long sum (int ... numbers) {
+public void analyze (File file) throws Exception { analyze(file, false); }
 
-    if (numbers == null) { throw new NullPointerException(); }
-    else if (numbers.length == 0) { throw new IllegalArgumentException(); }
-    else if (numbers.length == 1) { return numbers[0]; }
+///////////////////////////////////////////////////////////////////////////////
 
-    else {
+/**
+ * Глибокий аналіз DMF файлу
+ * @param file *.dmf файл
+ * @param deep використовувати глибокий аналіз файлу
+ * @throws Exception якщо в процесі аналізування сталася критична помилка
+ */
+public void analyze (File file, boolean deep) throws Exception {
 
-        long result = numbers[0];
+InflaterInputStream iis = null;
 
-        for (int z = 1; z < numbers.length; z++) {
-            result += numbers[z];
-        }
+// Починаємо зчитувати файл
+try (FileInputStream fis = new FileInputStream(file)) {
 
-        return result;
-    }    
+// Ініціалізація потоку зчитування даних
+is = fis;
+
+// Зчитування сигнатури DMF файлу
+signature = new Signature();
+signature.read();
+
+// Визначаємо тип сигнатури
+int signature_type = signature.getType();
+
+// Якщо файл стиснений, то створюємо декомпресійний потік
+if (signature_type == Signature.UNENCRYPTED_COMPRESSED ||
+    signature_type == Signature.ENCRYPTED_COMPRESSED) {
+
+    iis = new InflaterInputStream(fis);
+    is = iis;
+
+}
+
+// Зчитуємо блок заголовку
+header_structure = new Header_Structure();
+header_structure.read();
+if (deep) { header_structure.analyze(); }
+
+// Зчитуємо блок шарів
+layers_structure = new Layers_Structure();
+layers_structure.read();
+if (deep) { layers_structure.analyze(); }
+
+// Зчитуємо блок параметрів
+params_structure = new Params_Structure();
+params_structure.read();
+if (deep) { params_structure.analyze(); }
+
+// Зчитуємо блок умовних знаків
+symbols_structure = new Symbols_Structure();
+symbols_structure.read();
+if (deep) { symbols_structure.analyze(); }
+
+// Якщо файл містить паролі, то зчитуємо блок паролів
+if (signature_type == Signature.ENCRYPTED_UNCOMPRESSED ||
+    signature_type == Signature.ENCRYPTED_COMPRESSED) {
+
+    passwords_structure = new Passwords_Structure();
+    passwords_structure.read();
+    if (deep) { passwords_structure.analyze(); }
+
+}
+
+// Зчитуємо блок об'єктів
+objects_structure = new Objects_Structure();
+objects_structure.read();
+if (deep) { objects_structure.analyze(); }
+
+}
+
+catch (Exception e) { 
+    switch (e.getMessage()) {
+        case "D" -> { throw new Exception("DMF deep analyze error"); }
+        default  -> { throw new Exception("DMF analyze error"); } } }
+
+finally { // Закриваємо декомпресійний потік, якщо він ініціалізований
+          if (iis != null) { iis.close(); }
+          // Закриваємо основний потік зчитування даних
+          is.close(); }
+
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Getter'и - повертають складові блоки DMF файлу /////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Отримання сигнатури
+ * @return сигнатура
+ */
+public Signature getSignature()
+    { return signature; }
+
+///////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Отримання блоку заголовку
+ * @return блок заголовку
+ */
+public Header_Structure getHeaderStructure()
+    { return header_structure; }
+
+///////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Отримання блоку шарів
+ * @return блок шарів
+ */
+public Layers_Structure getLayersStructure()
+    { return layers_structure; }
+
+///////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Отримання блоку параметрів
+ * @return блок параметрів
+ */
+public Params_Structure getParamsStructure()
+    { return params_structure; }
+
+///////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Отримання блоку умовних знаків
+ * @return блок умовних знаків
+ */
+public Symbols_Structure getSymbolsStructure()
+    { return symbols_structure; }
+
+///////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Отримання блоку паролів
+ * @return блок паролів
+ */
+public Passwords_Structure getPasswordsStructure()
+    { return passwords_structure; }
+
+///////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Отримання блоку об'єктів
+ * @return блок об'єктів
+ */
+public Objects_Structure getObjectsStructure()
+    { return objects_structure; }
+
+///////////////////////////////////////////////////////////////////////////////
+// Базовий абстрактний клас, який представляє довільний блок DMF файлу ////////
+///////////////////////////////////////////////////////////////////////////////
+
+private abstract class Basic_Structure {
+
+protected int    structure_size = -1;
+protected byte[] structure_data = null;
+
+protected final ByteArrayOutputStream raw_data = new ByteArrayOutputStream();
+
+///////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Зчитування блоку даних
+ * @throws Exception якщо в процесі зчитування сталася критична помилка
+ */
+public void read() throws Exception {
+
+try { byte[] size = readAndCollectBytes(4);
+      structure_size = getInteger(size);
+      raw_data.write(size);
+
+      structure_data = readAndCollectBytes(structure_size);
+      raw_data.write(structure_data); }
+
+catch (IOException e) { throw new Exception(); }
+
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 
 /**
- * Метод підраховує добуток вхідних чисел
- * @param numbers масив/перелік вхідних чисел
- * @return добуток вхідних чисел
+ * Аналізування блоку даних
+ * @throws Exception якщо в процесі аналізування сталася критична помилка
  */
-public static long product (int ... numbers) {
+public void analyze() throws Exception {}
 
-    if (numbers == null) { throw new NullPointerException(); }
-    else if (numbers.length == 0) { throw new IllegalArgumentException(); }
-    else if (numbers.length == 1) { return numbers[0]; }
+///////////////////////////////////////////////////////////////////////////////
 
-    else {
+/**
+ * Отримання розміру блоку даних
+ * @return розмір блоку даних
+ */
+public int getStructureSize() { return structure_size; }
 
-        long result = numbers[0];
+///////////////////////////////////////////////////////////////////////////////
 
-        for (int z = 1; z < numbers.length; z++) {
-            result *= numbers[z];
-        }
+/**
+ * Отримання блоку даних
+ * @return блок даних
+ */
+public byte[] getStructureData() { return structure_data; }
 
-        return result;
-    }
+///////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Отримання сирих (необроблених) даних
+ * @return сирі (необроблені) дані
+ */
+public byte[] getRawData() { return raw_data.toByteArray(); }
+
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Signature - мітка розпізнавання DMF файлу //////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+/** Сигнатура - мітка розпізнавання DMF файлу */
+public class Signature {
+
+/** Нестиснений та незапаролений файл */
+public static final int UNENCRYPTED_UNCOMPRESSED = 0;
+/** Стиснений   та незапаролений файл */
+public static final int UNENCRYPTED_COMPRESSED = 1;
+/** Нестиснений та запаролений файл */
+public static final int ENCRYPTED_UNCOMPRESSED = 2;
+/** Стиснений   та запаролений файл */
+public static final int ENCRYPTED_COMPRESSED = 3;
+
+// ............................................................................
+
+private byte[] signature_data = null;
+
+///////////////////////////////////////////////////////////////////////////////
+
+/** Зчитування сигнатури */
+public void read() { signature_data = readBytes(32);
+                     collectBytes(signature_data); }
+
+///////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Отримання типу сигнатури
+ * @return UNENCRYPTED_UNCOMPRESSED, UNENCRYPTED_COMPRESSED, 
+ * ENCRYPTED_UNCOMPRESSED або ENCRYPTED_COMPRESSED
+ */
+public int getType() {
+    
+    int encryption_type;
+    int compression_type;
+    
+    if (signature_data[26] == 53) { encryption_type  = 1; }
+    else                          { encryption_type  = 0; }
+
+    if (signature_data[28] == 67) { compression_type = 1; }
+    else                          { compression_type = 0; }
+    
+    return encryption_type * 2 + compression_type;
+
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 
 /**
- * Метод рахує середнє арифметичне значення
- * @param numbers масив/перелік вхідних чисел
- * @return середнє арифметичне значення
+ * Задання типу сигнатури
+ * @param type UNENCRYPTED_UNCOMPRESSED, UNENCRYPTED_COMPRESSED, 
+ * ENCRYPTED_UNCOMPRESSED або ENCRYPTED_COMPRESSED
  */
-public static double arithmeticMean (int ... numbers) {
+public void setType (int type) {
 
-    if (numbers == null) { throw new NullPointerException(); }
-    else if (numbers.length == 0) { throw new IllegalArgumentException(); }
-    else if (numbers.length == 1) { return numbers[0]; }
-
-    else {
-
-        return sum(numbers) * 1d / numbers.length;
-
-    }
-}
+    switch (type) {
+        case UNENCRYPTED_UNCOMPRESSED -> { signature_data[26] = 48;
+                                           signature_data[28] = 32; }
+        case UNENCRYPTED_COMPRESSED   -> { signature_data[26] = 48;
+                                           signature_data[28] = 67; }
+        case ENCRYPTED_UNCOMPRESSED   -> { signature_data[26] = 53;
+                                           signature_data[28] = 32; }
+        default                       -> { signature_data[26] = 53;
+                                           signature_data[28] = 67; } } }
 
 ///////////////////////////////////////////////////////////////////////////////
 
 /**
- * Метод рахує середнє квадратичне значення
- * @param numbers масив/перелік вхідних чисел
- * @return середнє квадратичне значення
+ * Отримання сирих (необроблених) даних
+ * @return сирі (необроблені) дані
  */
-public static double quadraticMean (int ... numbers) {
+public byte[] getRawData() { return signature_data; }
 
-    if (numbers == null) { throw new NullPointerException(); }
-    else if (numbers.length == 0) { throw new IllegalArgumentException(); }
-    else if (numbers.length == 1) { return numbers[0]; }
+}
 
-    else {
+///////////////////////////////////////////////////////////////////////////////
 
-        for (int z = 0; z < numbers.length; z++)
-            { numbers[z] *= numbers[z]; }
-        
-        return Math.sqrt(sum(numbers) * 1d / numbers.length);
-    }
+/** Блок заголовку - мітить загальну інформацію про файл */
+public class Header_Structure extends Basic_Structure {}
+
+// ............................................................................
+
+/** Блок шарів - містить інформацію про всі шари */
+public class Layers_Structure extends Basic_Structure {}
+
+// ............................................................................
+
+/** Блок параметрів - містить інформацію про всі параметри */
+public class Params_Structure extends Basic_Structure {}
+
+// ............................................................................
+
+/** Блок умовних знаків - містить інформацію про всі умовні знаки */
+public class Symbols_Structure extends Basic_Structure {}
+
+// ............................................................................
+
+/** Блок паролів - містить зашифровану інформацію про паролі */
+public class Passwords_Structure extends Basic_Structure {}
+
+// ............................................................................
+
+/** Блок об'єктів - список усіх об'єктів разом із їхніми властивостями */
+public class Objects_Structure extends Basic_Structure {
+
+@Override
+public void read() throws Exception {
+
+while (is.available() > 0) {
+
+    byte[] size = readAndCollectBytes(4);
+    structure_size = getInteger(size);
+    
+    raw_data.write(size);
+    raw_data.write(readAndCollectBytes(structure_size));
+    
+}
+
+structure_size = raw_data.size();
+structure_data = raw_data.toByteArray();
+
+}
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Допоміжні методи ///////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+// Читання байтів
+private byte[] readBytes (int lenght)
+    { return readBytes(lenght, false); }
+
+// ............................................................................
+
+// Читання байтів із можливістю їх виведення в консоль
+private byte[] readBytes (int lenght, boolean print) {
+
+try { var data = is.readNBytes(lenght); 
+      if (print) { printBytes(data); }
+      return data; }
+
+catch (IOException e) { return null; } }
+
+///////////////////////////////////////////////////////////////////////////////
+
+// Запис байтів
+private void collectBytes (byte[] data)
+    { collectBytes(data, false); }
+
+// ............................................................................
+
+// Запис байтів із можливістю їх виведення в консоль
+private void collectBytes (byte[] data, boolean print) {
+    
+    try { temp.write(data); }
+    catch (IOException e) {  }
+    
+    if (print) { printBytes(data); } }
+
+///////////////////////////////////////////////////////////////////////////////
+
+// Читання та запис байтів
+private byte[] readAndCollectBytes (int lenght)
+    { return readAndCollectBytes(lenght, false); }
+
+// ............................................................................
+
+// Читання та запис байтів із можливістю їх виведення в консоль
+private byte[] readAndCollectBytes (int lenght, boolean print) {
+
+    var data = readBytes(lenght, print);
+    collectBytes(data);
+    
+    return data; }
+
+///////////////////////////////////////////////////////////////////////////////
+
+// Виведення байтів у консоль
+public void printBytes (byte[] bytes) { printBytes(bytes, true); }
+
+// ............................................................................
+
+// Виведення байтів у консоль із можливість вимкнути перенесення нового рядка
+public void printBytes (byte[] bytes, boolean nextLine) {
+    for (byte b : bytes) { System.out.print(byteToHex(b)); }
+    if (nextLine) { System.out.println(); }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+// Конвертування байту в HEX-строку
+private String byteToHex (byte b) {
+    return String.format("%02X", b & 0xff) + " ";
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+// Конвертування масиву байт в ціле число
+private int getInteger (byte[] data) {
+    return ByteBuffer.wrap(data).order(LITTLE_ENDIAN).getInt();
+}
+
+// ............................................................................
+
+// Конвертування масиву байт в строку
+private String getString (byte[] data) {
+    try { data = new String(data, "Windows-1251").getBytes();
+          return new String(data, "Windows-1251"); }
+    catch (UnsupportedEncodingException e) { return null; }
+
 }
 
 // Кінець класу DMF_Analyzer //////////////////////////////////////////////////
--- src/main/java/com/rutar/dmf_analyzer/DMF_Analyzer_Demo.java
+++ src/main/java/com/rutar/dmf_analyzer/DMF_Analyzer_Demo.java
@@ -2,14 +2,11 @@ package com.rutar.dmf_analyzer;
 
 import java.awt.*;
 import javax.swing.*;
-import java.awt.event.*;
-
-// ............................................................................
 
 /**
  * Клас DMF_Analyzer_Demo
  * @author Rutar_Andriy
- * 21.04.2024
+ * 01.06.2024
  */
 
 public class DMF_Analyzer_Demo extends JFrame {
@@ -20,124 +17,26 @@ public DMF_Analyzer_Demo() { initComponents(); }
 
 ///////////////////////////////////////////////////////////////////////////////
 
-private void initComponents() {
+@SuppressWarnings("unchecked")
+    private void initComponents() {//GEN-BEGIN:initComponents
 
-label_hint    = new JLabel();
-field_input   = new JTextField();
-btn_random    = new JButton();
-sep           = new JSeparator();
-panel_buttons = new JPanel();
-btn_sum       = new JButton();
-btn_product   = new JButton();
-btn_aMean     = new JButton();
-btn_qMean     = new JButton();
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-setTitle("DMF_Analyzer Demo");
-setPreferredSize(new Dimension(500, 91));
-setResizable(false);
+        GroupLayout layout = new GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            .addGap(0, 400, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            .addGap(0, 300, Short.MAX_VALUE)
+        );
 
-label_hint.setHorizontalAlignment(SwingConstants.CENTER);
-label_hint.setText("Введіть числа, розділяючи їх комою");
-
-field_input.setHorizontalAlignment(JTextField.CENTER);
-
-btn_random.setText("<html><body align=\"center\">Згенерувати<br/>випадкові числа</body></html>");
-btn_random.setToolTipText("<html><body align=\"center\">Згенерувати випадкову кількість<br/>випадкових чисел</body></html>");
-btn_random.setActionCommand("btn_random");
-btn_random.setMargin(new Insets(2, 9, 2, 9));
-btn_random.addActionListener(this::onButtonClick);
-
-panel_buttons.setLayout(new GridLayout(1, 0));
-
-btn_sum.setText("Сума");
-btn_sum.setToolTipText("Розрахувати суму введених чисел");
-btn_sum.setActionCommand("btn_sum");
-btn_sum.addActionListener(this::onButtonClick);
-panel_buttons.add(btn_sum);
-
-btn_product.setText("Добуток");
-btn_product.setToolTipText("Розрахувати добуток введених чисел");
-btn_product.setActionCommand("btn_product");
-btn_product.addActionListener(this::onButtonClick);
-panel_buttons.add(btn_product);
-
-btn_aMean.setText("x̅");
-btn_aMean.setToolTipText("Розрахувати середнє арифметичне значення");
-btn_aMean.setActionCommand("btn_aMean");
-btn_aMean.addActionListener(this::onButtonClick);
-panel_buttons.add(btn_aMean);
-
-btn_qMean.setText("x(rms)");
-btn_qMean.setToolTipText("Розрахувати середнє квадратичне значення");
-btn_qMean.setActionCommand("btn_qMean");
-btn_qMean.addActionListener(this::onButtonClick);
-panel_buttons.add(btn_qMean);
-
-GroupLayout layout = new GroupLayout(getContentPane());
-getContentPane().setLayout(layout);
-layout.setHorizontalGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-    .addGroup(layout.createSequentialGroup()
-        .addGap(5, 5, 5)
-        .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-            .addComponent(sep)
-            .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-                    .addComponent(label_hint, GroupLayout.DEFAULT_SIZE, 382, Short.MAX_VALUE)
-                    .addComponent(field_input))
-                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(btn_random, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-            .addComponent(panel_buttons, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-        .addGap(5, 5, 5))
-);
-layout.setVerticalGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-    .addGroup(layout.createSequentialGroup()
-        .addGap(5, 5, 5)
-        .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING, false)
-            .addComponent(btn_random)
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(label_hint)
-                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(field_input, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))
-        .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-        .addComponent(sep, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-        .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-        .addComponent(panel_buttons, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-        .addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-);
-
-pack();
-setLocationRelativeTo(null);
-
-}
+        pack();
+    }//GEN-END:initComponents
 
 ///////////////////////////////////////////////////////////////////////////////
 
-private void onButtonClick (ActionEvent evt) {
-    
-    String command = ((JButton)evt.getSource()).getActionCommand();
-    
-    if (command.equals("btn_random")) { generateRandomNumbers();
-                                        return; }
-    
-    int checkInput = checkInput();
-    if (checkInput > 0) { showErrorMessage(checkInput);
-                          return; }
-    
-    switch (command) {
-        case "btn_sum"     -> showResultMessage(0);
-        case "btn_product" -> showResultMessage(1);
-        case "btn_aMean"   -> showResultMessage(2);
-        case "btn_qMean"   -> showResultMessage(3);
-    }
-}
-
-///////////////////////////////////////////////////////////////////////////////
-
-public static void main(String args[]) {
-
-    // Правила оформлення проектів описані тут:
-    // https://github.com/RutarAndriy/My_Coding_Rules
+public static void main (String args[]) {
 
     EventQueue.invokeLater(() -> {
         new DMF_Analyzer_Demo().setVisible(true);
@@ -146,130 +45,8 @@ public static void main(String args[]) {
 
 ///////////////////////////////////////////////////////////////////////////////
 
-private JLabel     label_hint;
-private JTextField field_input;
-private JButton    btn_random;
-private JSeparator sep;
-private JPanel     panel_buttons;
-private JButton    btn_sum;
-private JButton    btn_product;
-private JButton    btn_aMean;
-private JButton    btn_qMean;
-
-///////////////////////////////////////////////////////////////////////////////
-
-private int checkInput() {
-
-    String input = field_input.getText().replaceAll(" ", "");
-
-    if (input.isBlank())     { return 1; }
-    if (input.endsWith(",")) { return 2; }
-
-    String[] split = input.split(",");
-
-    for (String value : split) {
-        try { double D = Double.parseDouble(value);
-              int    I = Integer.parseInt(value);
-              if (D - I > 0)            { return 2; } }
-        catch (NumberFormatException e) { return 2; }
-    }
-
-    return 0;
-
-}
-
-///////////////////////////////////////////////////////////////////////////////
-
-private int[] getNumbers() {
-    
-    String input = field_input.getText().replaceAll(" ", "");
-    String[] split = input.split(",");
-    int[] numbers = new int[split.length];
-
-    for (int z = 0; z < split.length; z++) {
-        numbers[z] = Integer.parseInt(split[z]);
-    }
-
-    return numbers;
-
-}
-
-///////////////////////////////////////////////////////////////////////////////
-
-private void generateRandomNumbers() {
-            
-    int random;
-    String input;
-
-    field_input.setText(null);
-    int count = (int)(Math.random() * 7) + 5;
-
-    for (int z = 0; z < count; z++) {
-        random = (int)(Math.random() * 40) - 10;
-        input = field_input.getText();
-        field_input.setText(input + (input.isEmpty() ? "" : ", ") + random);
-    }
-}
-
-///////////////////////////////////////////////////////////////////////////////
-
-private void showResultMessage (int resultCode) {
-
-    String text;
-    String message;
-    Number result;
-
-    Color background = field_input.getBackground();
-    field_input.setBackground(new Color(0xff66));
-
-    String numbers = field_input.getText().replaceAll(" ", "")
-                                          .replaceAll(",", ", ");
-
-    field_input.setText(numbers);
-
-    switch (resultCode) {
-        case 0 ->
-            { text = "Сума чисел\n(%s)\nдорівнює %d";
-              result = DMF_Analyzer.sum(getNumbers()); }
-        case 1 ->
-            { text = "Добуток чисел\n(%s)\nдорівнює %d";
-              result = DMF_Analyzer.product(getNumbers()); }
-        case 2 ->
-            { text = "Середнє арифметичне значення чисел\n(%s)\nдорівнює %.3f";
-              result = DMF_Analyzer.arithmeticMean(getNumbers()); }
-        default ->
-            { text = "Середнє квадратичне значення чисел\n(%s)\nдорівнює %.3f";
-              result = DMF_Analyzer.quadraticMean(getNumbers()); }
-    }
-
-    message = String.format(text, numbers, result);
-    JOptionPane.showMessageDialog(btn_product, message,
-                                 "Рузультат", JOptionPane.PLAIN_MESSAGE);
-
-    field_input.setBackground(background);
-
-}
-
-///////////////////////////////////////////////////////////////////////////////
-
-private void showErrorMessage (int errorCode) {
-
-    Color background = field_input.getBackground();
-    field_input.setBackground(new Color(0xff6666));
-
-    String message = null;
-
-    switch (errorCode) {
-        case 1 -> message = "Поле введення не\nмістить жодних даних";
-        case 2 -> message = "Помилка вводу.\nДопускаються тільки цілі числа";
-    }
-
-    JOptionPane.showMessageDialog(btn_product, message,
-                                 "Помилка", JOptionPane.ERROR_MESSAGE);
-
-    field_input.setBackground(background);
-
-}
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // End of variables declaration//GEN-END:variables
 
 // Кінець класу DMF_Analyzer_Demo /////////////////////////////////////////////
 
